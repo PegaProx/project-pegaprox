39d41ba3ce6d74e51df2752de0d9acf5c78e2494
MrMasterbay <gamesucht1236@gmail.com>
2026-06-30 22:59:37 +0200

    chore(release): bump to Beta 0.9.13.3
    Snapshot schedules (#586) + replication overview tab (#430) + a stack of
    bug-fixes (#594/#597/#592/#555/#590/#585/#584/#583/#580/#587) and the CIS
    hardening log-bounds (#595). Tag/release after the #595 on-node smoke test.

--- pegaprox/constants.py
+++ pegaprox/constants.py
@@ -8,8 +8,8 @@ import os
 from pathlib import Path
 
 # Version
-PEGAPROX_VERSION = "Beta 0.9.13.2"
-PEGAPROX_BUILD = "2026.06.24"
+PEGAPROX_VERSION = "Beta 0.9.13.3"
+PEGAPROX_BUILD = "2026.06.30"
 
 # File Paths & Directories
 CONFIG_DIR = 'config'
